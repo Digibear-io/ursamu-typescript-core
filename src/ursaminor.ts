30740e5e4900f91b2897c6a463e225ae8d1f8f80
= <=>
2020-03-12 09:36:21 -0700

    Tutorial capture 2

--- src/ursaminor.ts
+++ src/ursaminor.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import { Server } from "http";
 import socketio, { Socket } from "socket.io";
 import { fork } from "child_process";
+import config from "../config/config.json";
 
 // Define the various communication channels.
 const app = express();
@@ -9,15 +10,19 @@ const server = new Server(app);
 const io = socketio(server);
 
 // Boot UrsaMU communication layer and mush engine.
-const engine = fork("./src/engine.ts");
-server.listen(8080, () => console.log("Server started."));
+const ursaMajor = fork("./src/ursamajor.ts");
 
 app.get("/", (req: Request, res: Response) =>
   res.send(`<h1>Hello World!</h1>`)
 );
-engine.send("Foobar!");
-io.on("connection", (socket: Socket) => {
+
+// Handle new connections to the
+io.on("connection", (socket: Socket) =>
   socket.on("message", (data: string) =>
-    engine.send(JSON.stringify({ id: socket.id, payload: data }))
-  );
-});
+    ursaMajor.send(JSON.stringify({ id: socket.id, text: data }))
+  )
+);
+
+server.listen(config.game.port, () =>
+  console.log(`Server started on port: ${config.game.port}`)
+);
