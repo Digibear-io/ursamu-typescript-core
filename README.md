f78e219361dfbf9e9e93164f7a6f3ddca68f2072
= <=>
2020-03-11 18:18:12 -0700

    Reorganizing some.  General dev on the server

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# ursamu
+# UrsaMU
 
-A Typescript based MUSH-like server
+![Repo Cover Image](./ursamu_github_banner.png)
+
+## What is UrsaMU?
+
+**UrsaMU** is a **modern implementation** of the **MUSH** style internet talker, built on the shoulders of some great developers and impimented with Typescript. As it's name might imply, the default commands have a mush feel to them. However, every **command**, and every **softcode function** can be customized to fit your game!
+
+## Stay Tuned!
+
+**UrsaMU** is under heavy development. A stable version for testing is coming soon!
--- src/ursamu.ts
+++ src/ursamu.ts
@@ -3,18 +3,21 @@ import { Server } from "http";
 import socketio, { Socket } from "socket.io";
 import { fork } from "child_process";
 
+// Define the various communication channels.
 const app = express();
 const server = new Server(app);
 const io = socketio(server);
 
+// Boot UrsaMU communication layer and mush engine.
 const engine = fork("./src/engine.ts");
-engine.send("Foobar!!");
 server.listen(8080, () => console.log("Server started."));
 
 app.get("/", (req: Request, res: Response) =>
   res.send(`<h1>Hello World!</h1>`)
 );
-
+engine.send("Foobar!");
 io.on("connection", (socket: Socket) => {
-  socket.on("message", (data: string) => engine.send(JSON.stringify(data)));
+  socket.on("message", (data: string) =>
+    engine.send(JSON.stringify({ id: socket.id, payload: data }))
+  );
 });
